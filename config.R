4bec239347fba35a09a40ec0e0e67dd4f870fdd4
John Helveston <john.helveston@gmail.com>
2020-02-19 19:20:47 -0500

    removed unncessary libraries

--- config.R
+++ config.R
@@ -1,14 +1,13 @@
 library(tibble)
 library(shiny)
 library(shinyWidgets)
-library(shinyjs)
 library(magick)
 # library(ggthemes)
 
 # Define keyboards
 keyboards <- tribble(
     ~id,         ~name,
-    'ergodox',   'Ergodox', 
+    'ergodox',   'Ergodox',
     'ergodash2', 'Ergodash 2',
     'ergodash1', 'Ergodash 1',
     'redox',     'Redox',
@@ -22,8 +21,8 @@ keyboards <- tribble(
 # Add color palette
 # color <- gdocs_pal()(nrow(keyboards))
 # color <- hue_pal()(nrow(keyboards))
-# Dark2 from RColorBrewer: 
-colors <- c("#1B9E77", "#D95F02", "#D95F02", "#7570B3", "#E7298A", 
+# Dark2 from RColorBrewer:
+colors <- c("#1B9E77", "#D95F02", "#D95F02", "#7570B3", "#E7298A",
             "#66A61E", "#E6AB02", "#A6761D", "#FFFFFF")
 labels <- paste0("color: ", colors)
 keyboards$color <- colors
