09257018c792365097b22015ffb4bee7ec3bebe8
John Paul Helveston <john.helveston@gmail.com>
2020-02-20 21:42:28 -0500

    added some higher contrast colors

--- config.R
+++ config.R
@@ -2,7 +2,6 @@ library(tibble)
 library(shiny)
 library(shinyWidgets)
 library(magick)
-# library(ggthemes)
 
 # Define keyboards
 keyboards <- tribble(
@@ -19,11 +18,20 @@ keyboards <- tribble(
 )
 
 # Add color palette
-# color <- gdocs_pal()(nrow(keyboards))
-# color <- hue_pal()(nrow(keyboards))
+# library(ggthemes)
+# palette <- gdocs_pal()(nrow(keyboards))
 # Dark2 from RColorBrewer:
-colors <- c("#1B9E77", "#D95F02", "#D95F02", "#7570B3", "#E7298A",
-            "#66A61E", "#E6AB02", "#A6761D", "#FFFFFF")
+# palette <- c("#1B9E77", "#D95F02", "#7570B3", "#E7298A",
+#             "#66A61E", "#E6AB02", "#A6761D", "#FFFFFF")
+# Colors from here: https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+palette <- c('#4363d8', '#3cb44b', '#ffe119', '#e6194b', '#f58231',
+             '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
+             '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000',
+             '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080',
+             '#ffffff', '#000000')
+colors <- palette
+colors <- c(colors[1], rep(colors[2], 2), colors[3:length(colors)])
+colors <- colors[1:nrow(keyboards)]
 labels <- paste0("color: ", colors)
 keyboards$color <- colors
 keyboards$label <- labels
