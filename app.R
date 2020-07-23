01b2a2e04702c1e380ce710d0fda227e5372a7c1
John Helveston <john.helveston@gmail.com>
2020-07-22 21:32:32 -0400

    added ferris keyboard

--- app.R
+++ app.R
@@ -134,7 +134,7 @@ server <- function(input, output, session) {
             escape = FALSE,
             style = 'bootstrap',
             rownames = FALSE,
-            options = list(pageLength = 30))
+            options = list(pageLength = 50))
     })
 
     # Control reset button
