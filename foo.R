68e4919f5e681a9b2aac7568669ef212531e8e4d
John Helveston <john.helveston@gmail.com>
2021-05-23 21:06:54 -0400

    Delete foo.R

--- foo.R
+++
@@ -1,7 +0,0 @@
-source(file.path('code', 'config.R'))
-source(file.path('code', 'functions.R'))
-
-
-board_svg <- rsvg::rsvg_raw(here::here('images', 'svg', 'squiggle.svg'), width = 2000)
-board_png <- magick::image_read(board_svg)
-
